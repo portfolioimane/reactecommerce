ec5460422850fb637d45c654d3b116f9fa6436f0
portfolioimane <portfolioimane.g@gmail.com>
2021-07-08 23:27:44 +0200

    first commit

---
+++ src/components/Cart/CartItem/styles.js
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Autorenew } from '@material-ui/icons';
+
+export default makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  title: {
+    marginTop: '5%',
+  },
+  emptyButton: {
+    minWidth: '150px',
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '5px',
+    },
+    [theme.breakpoints.up('xs')]: {
+      marginRight: '20px',
+    },
+  },
+  checkoutButton: {
+    minWidth: '150px',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+  cardDetails: {
+    display: 'flex',
+    marginTop: '10%',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  }
+}));
